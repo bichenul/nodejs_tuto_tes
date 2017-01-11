655a48aad04929ed3ce0e6514bfbfd7f39300870
bichenul <nicolas@lenec.fr>
2017-01-11 16:05:21 +0100

    les routes
    update

--- serveur.js
+++ serveur.js
@@ -17,4 +17,16 @@ app.get('/etage/1/chambre', function(req, res) {
     res.end('Hé ho, c\'est privé ici !');
 });
 
+//avec parametre
+app.get('/etage/:etagenum/chambre', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à la chambre de l\'étage n°' + req.params.etagenum);
+});
+
+//hestion des erreurs 404
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(404, 'Page introuvable !');
+});
+
 app.listen(8080);
