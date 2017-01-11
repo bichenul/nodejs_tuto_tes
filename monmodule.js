20d433322045a105f7980318e380e1333ebfa31e
bichenul <nicolas@lenec.fr>
2017-01-11 15:00:38 +0100

    change a word
    update

---
+++ monmodule.js
@@ -0,0 +1,10 @@
+var direBonjour = function() {
+    console.log('Bijour !');
+}
+
+var direByeBye = function() {
+    console.log('Bye bye !');
+}
+
+exports.direBonjour = direBonjour;
+exports.direByeBye = direByeBye;
