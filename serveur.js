51ffd949c75fa2a4365df4b6842cb4bd9f20f14a
bichenul <nicolas@lenec.fr>
2017-01-11 16:12:38 +0100

    tutoriel nodejs
    utilisation template ejs

--- serveur.js
+++ serveur.js
@@ -17,10 +17,9 @@ app.get('/etage/1/chambre', function(req, res) {
     res.end('Hé ho, c\'est privé ici !');
 });
 
-//avec parametre
+//Nous pouvons maintenant déléguer la gestion de la vue (du HTML) à notre moteur de template. Plus besoin d'écrire du HTML au milieu du code JavaScript comme un cochon !
 app.get('/etage/:etagenum/chambre', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Vous êtes à la chambre de l\'étage n°' + req.params.etagenum);
+    res.render('chambre.ejs', {etage: req.params.etagenum});
 });
 
 //hestion des erreurs 404
